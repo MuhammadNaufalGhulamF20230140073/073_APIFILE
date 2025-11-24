6e18798f11c6c92dcad67ccfd05b226c55dcb43e
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2025-11-24 09:33:29 +0700

    membuat index

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const connectDatabase = require("/config/db");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+async function startServer() {
+  await connectDatabase();
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
+}
+startServer();
